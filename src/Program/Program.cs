7885c7c6e241a98d05398988678218cf7f1767cd
nierjero <jeronimo.gonzalez@correo.ucu.edu.uy>
2023-10-18 08:12:36 -0300

    telefonos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -11,27 +11,25 @@ namespace Program
         static void Main(string[] args)
         {
             var whatsApp = new WhatsAppApi();
-            whatsApp.Send("+598<Poné tu teléfono acá>", "Hey! I'm using WhatsApp");
+            whatsApp.Send("+598 091785770", "Hey! I'm using WhatsApp");
            
             // Crear el dueño del teléfono
-            Contact owner = new Contact("Dueño");
+            Contact owner = new Contact("Dueño","+598 091785770");
 
             // Crear una lista de contactos (Phonebook)
             Phonebook phonebook = new Phonebook(owner);
 
             // Crear y agregar contactos a la lista
-            Contact contact1 = new Contact("Amigo 1");
-            Contact contact2 = new Contact("Amigo 2");
-
+            Contact contact1 = new Contact("Amigo 1", "+598 091785770");
+            
             // Agregar contactos a la lista
             phonebook.AddContact(contact1);
-            phonebook.AddContact(contact2);
 
             // Enviar un mensaje por WhatsApp a algunos contactos
             WhatsAppChannel whatsappChannel = new WhatsAppChannel();
 
             Contact sender = owner; // El remitente es el dueño
-            string[] recipientNames = { "Amigo 1", "Amigo 2" };
+            string[] recipientNames = { "Amigo 1" };
             string messageText = "¡Hola desde WhatsApp!";
 
             List<Contact> recipient = phonebook.Search(recipientNames);
